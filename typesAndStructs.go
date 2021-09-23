0c17c51d9af3ded3ff96cc32752fcb8a616c06bc
TheOryZ <kadirrturan@gmail.com>
2021-09-24 00:37:37 +0300

    Added Sturct and types
    Added Sturct and types

---
+++ typesAndStructs.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var s = "seven"
+
+func main() {
+	s := "eight"
+	/*Aynı isimde bir başka değişkeni, alt fonksiyonlarda tanımlayabilirsin.
+	Daha içteki işlem bloğundaki tanımlama baskın gelecektir.
+	Tanımlama yaparken dikkatli isim ver!!*/
+	var s2 = "six"
+	fmt.Println("s is", s)
+	fmt.Println("s2 is", s2)
+
+	saySomething(("xxx"))
+
+	/* ---TYPES--- */
+	user := user{
+		FirstName: "John",
+		LastName:  "Doe",
+	}
+
+	fmt.Println(user.FirstName, user.LastName, user.BirthDate, user.Age)
+}
+
+/*Erişim belirleyici keyleri (public, private vs.) yoktur.
+Eğer obje büyük harf ile başlarsa Public
+Eğer obje küçük harf ile başlarsa Private
+*/
+func saySomething(s string) (string, string) {
+	fmt.Println("s from the saySomethings func is", s)
+	return s, "World"
+}
+
+type user struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
