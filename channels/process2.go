5cdfbacaaac69d6f7c4ee1c452d27bb30e4b9827
Kadir Turan <kadirrturan@gmail.com>
2021-09-30 10:52:26 +0300

    Added new channels examples
    Added new channels examples

---
+++ channels/process2.go
@@ -0,0 +1,31 @@
+package channels
+
+import (
+	"fmt"
+	"time"
+)
+
+//We’ll use select to await both of these values simultaneously, printing each one as it arrives.
+func Process2() {
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
---
+++ channels/process3.go
@@ -0,0 +1,35 @@
+package channels
+
+import (
+	"fmt"
+	"time"
+)
+
+// Running this program shows the first operation timing out and the second succeeding.
+func Process3() {
+
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
+
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second): //time.After
+		fmt.Println("timeout 1")
+	}
+
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
+	}()
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second): //time.After
+		fmt.Println("timeout 2")
+	}
+}
