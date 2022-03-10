4de9cdfa5635553ae02b066c39d724c1df576632
TheOryZ <kadirrturan@gmail.com>
2022-03-11 01:27:30 +0300

    --comm
    --comm

--- Goroutin/main.go
+++ Goroutin/main.go
@@ -1,15 +1,27 @@
 package main
 
-import "time"
+import (
+	"runtime"
+	"time"
+)
 
 func main() {
 	// basit gorouting kullanımı
 	go xFunc()
 	time.Sleep(100 * time.Millisecond)
-	//...
+
+	runtime.GOMAXPROCS(1)
+	go zFunc()
+	time.Sleep(100 * time.Millisecond)
 }
 func xFunc() {
 	for i := byte('a'); i <= byte('z'); i++ {
 		println(string(i))
+	}
+}
+
+func zFunc() {
+	for i := byte('a'); i <= byte('z'); i++ {
+		go println(string(i))
 	}
 }
